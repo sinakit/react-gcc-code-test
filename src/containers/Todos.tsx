7129a783d522280894dec0530af562d40fd2b7ae
sinakit <hin.sinak@gmail.com>
2021-08-31 08:58:54 +0700

    2 files modified

--- src/containers/Todos.tsx
+++ src/containers/Todos.tsx
@@ -1,16 +1,24 @@
-import * as React from "react";
+import * as React from 'react';
 
-import { TodoContext } from "../context/todoContext";
-import Todo from "../components/Todo";
+import { TodoContext } from '../context/todoContext';
+import Todo from '../components/Todo';
 
 const Todos = () => {
-  const { todos, updateTodo, findTodo, removeTodo } = React.useContext(TodoContext) as ContextType;
+  const { todos, updateTodo, findTodo, removeTodo } = React.useContext(
+    TodoContext
+  ) as ContextType;
   return (
-    <>
+    <React.Fragment>
       {todos.map((todo: ITodo) => (
-        <Todo key={todo.id} updateTodo={updateTodo} todo={todo} findTodo={findTodo} removeTodo={removeTodo} />
+        <Todo
+          key={todo.id}
+          updateTodo={updateTodo}
+          todo={todo}
+          findTodo={findTodo}
+          removeTodo={removeTodo}
+        />
       ))}
-    </>
+    </React.Fragment>
   );
 };
 
